8d8a7b81fcc28991ce5e318a194e6e39970ab739
Aaron Bernstein <aaron@stitchmedia.ca>
2015-10-03 19:10:47 -0400

    generic functions

--- test/test.hx
+++ test/test.hx
@@ -4,10 +4,10 @@ import haxe.foo;
 class foo extends bar implements baz {
     typedef foo {}
     interface foo {}
-    
+
     function new(){
     }
-    
+
     //region foo
     //endregion
     var foo = function () {}
@@ -19,7 +19,40 @@ class foo extends bar implements baz {
     static function foo() {}
     private function foo() {}
     public function foo() {}
-    public static function foo() {}
+    public static inline function foo() {}
     private static function foo() {}
     var foo:Bar = "baz";
+
+
+    class Array<T> {
+        function push(x : T) : Int;
+    }
+
+    @:generic static function method<T>(t:T) { }
+
+    public static function equals<T> (expected:T, actual:T) {
+        if (actual != expected) {
+            trace('$actual should be $expected');
+        }
+    }
+
+    enum Color {
+        Red;
+        Green;
+        Blue;
+        Rgb(r:Int, g:Int, b:Int);
+    }
+
+    @:generic
+    static function make<T:Constructible>():T {
+        return new T("foo");
+    }
+
+    /*
+    * function new () 
+    */
+
+    // ignore this comment with the word class in it
+
+
 }
