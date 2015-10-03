6284fc4c0c71d2b27fc1f553316ebca34615a340
Jan Pilzer <jan.pilzer@gmx.de>
2015-10-03 20:15:24 +0200

    Add Python test file

---
+++ test/test.py
@@ -0,0 +1,13 @@
+class TestClass(SuperClass):
+    def publicMethod(self, arg1):
+        pass
+
+    def __privateMethod(self, arg1):
+        pass
+
+class Test:
+    def publicMethod(self, arg1):
+        pass
+
+def firstFunction(arg1, arg2):
+    pass
