b90e4ca5922e198494bfff0a1b7d3ab553399918
edumardo <aesalemolo@gmail.com>
2015-11-22 22:30:01 +0000

    Spanish translation

---
+++ nls/es/strings.js
@@ -0,0 +1,24 @@
+define({
+    HEADER_TITLE: "Outline",
+
+    TOOLBAR_ICON_TOOLTIP: "Mostrar/Ocultar Outline",
+
+    BUTTON_SETTINGS: "Configurar Outline",
+    BUTTON_MOVE: "Posición del panel Outline",
+    BUTTON_CLOSE: "Cerrar Outline",
+
+    COMMAND_SORT: "Ordenar funciones",
+    COMMAND_UNNAMED: "Mostrar funciones anónimas",
+    COMMAND_ARGS: "Mostrar argumentos",
+
+    PREF_ENABLED_NAME: "Outline - Habilitar",
+    PREF_ENABLED_DESC: "Habilitar/Deshabilitar Outline List",
+    PREF_UNNAMED_NAME: "Outline - Anónimas",
+    PREF_UNNAMED_DESC: "Mostrar/Ocultar funciones anónimas en Outline List",
+    PREF_ARGS_NAME: "Outline - Argumentos",
+    PREF_ARGS_DESC: "Mostrar/ocultar parametros de función y clases HTML en Outline List",
+    PREF_SIDEBAR_NAME: "Outline - Barra lateral",
+    PREF_SIDEBAR_DESC: "Marca para mostrar Outline List en la barra lateral bajo el arbol de archivos",
+    PREF_SORT_NAME: "Outline - Ordenar",
+    PREF_SORT_DESC: "Marca para ordenar el contenido de Outline List"
+});
