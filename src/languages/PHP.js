12b94516c21af7f032dd59628b6c0d9310235d5e
Wang Zhi <yfwz100@yeah.net>
2016-02-12 21:31:04 +0800

    Using lex to build a PHP parser.

--- src/languages/PHP.js
+++ src/languages/PHP.js
@@ -1,5 +1,7 @@
 define(function (require, exports, module) {
     "use strict";
+  
+    var Lexer = require('thirdparty/lex/lexer');
 
     var defaultVisibilty = "public";
     var unnamedPlaceholder = "function";
@@ -26,6 +28,126 @@ define(function (require, exports, module) {
             $html: $elements
         };
     }
+    
+    function parse(source) {
+        var lexer, literal, ns, line, peek, pop, results, state, ignored;
+        line = 0;
+        ns = [];
+        literal = true;
+        state = [];
+        peek = function(a) {
+            if (a.length > 0) {
+                return a[a.length - 1];
+            }
+        };
+        pop = function(a) {
+            if (a.length > 0) {
+                return a.pop();
+            }
+        }
+        results = [];
+        ignored = function () {};
+        lexer = new Lexer;
+        lexer.addRule(/\<\?(php)?/, function() {
+            literal = false;
+        }).addRule(/\?\>/, function() {
+            literal = true;
+        }).addRule(/function/, function(w) {
+            if (!literal) {
+                state.push('function');
+                results.push({
+                    type: 'function',
+                    name: ns.join('::'),
+                    args: [],
+                    modifier: 'unnamed',
+                    isStatic: false,
+                    line: line
+                });
+            }
+        }).addRule(/class/, function(w) {
+            if (!literal) {
+                state.push('class');
+                results.push({
+                    type: 'class',
+                    name: ns.join('::'),
+                    args: [],
+                    modifier: 'public',
+                    isStatic: false,
+                    line: line
+                });
+            }
+        }).addRule(/\$[a-zA-Z_]+/, function(w) {
+            if (peek(state) === 'args') {
+                peek(results).args.push(w);
+            }
+        }).addRule(/[a-zA-Z_]+/, function(w) {
+            var ref;
+            if (!literal) {
+                switch(peek(state)) {
+                    case 'function':
+                        ns.push(w);
+                        ref = peek(results);
+                        if (ns.length > 1) {
+                            ref.name += '::' + w;
+                        } else {
+                            ref.name = w;
+                        }
+                        ref.modifier = 'public';
+                        break;
+                    case 'class':
+                        ns.push(w);
+                        ref = peek(results);
+                        ref.name += '::' + w;
+                        break;
+                }
+          }
+        }).addRule(/\(/, function () {
+            if (peek(state) === 'function') {
+                var ref = peek(results);
+                if (!ref || ref.type !== 'function') {
+                    ns.push(unnamedPlaceholder);
+                    results.push({
+                        type: 'function',
+                        name: ns.join('::'),
+                        args: [],
+                        modifier: 'unnamed',
+                        isStatic: false,
+                        line: line
+                    });
+                }
+                state.push('args');
+            }
+        }).addRule(/\)/, function () {
+            if (peek(state) === 'args') {
+                state.pop();
+            }
+        }).addRule(/{/, function(w) {
+          var prefix, ref;
+          if (!literal) {
+            if ((ref = peek(state)) === 'function' || ref === 'class') {
+              prefix = state.pop();
+              state.push(prefix + ":start");
+            } else {
+              state.push('start');
+            }
+          }
+        }).addRule(/}/, function(w) {
+            var s;
+            if (!literal && state.length > 0) {
+                s = state.pop().split(':')[0];
+                if (s === 'function' || s === 'class') {
+                    ns.pop();
+                }
+            }
+        }).addRule(/./, ignored).addRule(/\n/, function () {
+            line += 1;
+        });
+        lexer.setInput(source);
+        while (lexer.index < source.length - 1) {
+            lexer.lex();
+        }
+        return results;
+    };
 
     /**
      * Create the entry list of functions language dependent.
@@ -35,29 +157,18 @@ define(function (require, exports, module) {
      * @returns {Array}   List of outline entries.
      */
     function getOutlineList(text, showArguments, showUnnamed) {
-        var lines = text.split("\n");
-        var regex = /(?:([\w$]+)\s*[=:]\s*)?(public|protected|private)?\s*(static)?\s*function(?:\s+(?:&\s*)?([\w]+))?\s*(\(.*\))/g;
-        var result = [];
-        lines.forEach(function (line, index) {
-            var match = regex.exec(line);
-            while (match !== null) {
-                var name = (match[1] || match[4] || "").trim();
-                var vis = match[2] || defaultVisibilty;
-                var isStatic = match[3] === "static";
-                var args = showArguments ? match[5] : "";
-                match = regex.exec(line);
-                if (name.length === 0) {
-                    if (showUnnamed) {
-                        name = unnamedPlaceholder;
-                        vis = "unnamed";
-                    } else {
-                        continue;
-                    }
-                }
-                result.push(createListEntry(name, args, vis, isStatic, index, line.length));
-            }
+        return parse(text).filter(function (it) {
+            return it.type === 'function';
+        }).map(function (it) {
+            return createListEntry(
+                it.name,
+                '(' + it.args.join(',') + ')',
+                it.modifier,
+                it.isStatic,
+                it.line,
+                0
+            );
         });
-        return result;
     }
 
     function compare(a, b) {
