e384560ea3b3c8cd4b140c9aa5e07b73674d215e
Jan Pilzer <jan.pilzer@gmx.de>
2016-04-11 23:00:01 +0200

    Add support for unnamed functions to the Parser

--- src/lexers/JSParser.js
+++ src/lexers/JSParser.js
@@ -6,22 +6,42 @@ define(function (require, exports, module) {
     /** @const {string} Placeholder for unnamed functions. */
     var UNNAMED_PLACEHOLDER = "function";
 
+    /**
+     * Parse an array of Identifier nodes and the corresponding default values.
+     * @private
+     * @param   {Indetifier[]} args     List of params
+     * @param   {Literal[]}    defaults List of default values
+     * @returns {string[]}     List of parsed strings for the arguments.
+     */
     function _parseArgs(args, defaults) {
         return args.map(function (arg, i) {
             return arg.name + (defaults[i] ? "=" + defaults[i].value : "");
         });
     }
 
+    /**
+     * Visit a node in the ast.
+     * If it is a node representing a function, return an object.
+     * If it is a node which can be the name of a following function, return a string.
+     * @private
+     * @param   {object}         node   AST node
+     * @param   {string}         [name] Name of a previous node
+     * @returns {object|boolean} Parsed function object, or name
+     */
     function _visit(node, name) {
         if (node.params) {
             name = node.id ? node.id.name : name || UNNAMED_PLACEHOLDER;
             var type;
             if (node.generator) {
                 type = "generator";
+            } else if (name === UNNAMED_PLACEHOLDER) {
+                type = "unnamed";
+            } else if (name[0] === "_") {
+                type = "private";
             } else if (name[0] === name[0].toUpperCase()) {
                 type = "class";
             } else {
-                type = "function";
+                type = "public";
             }
             return {
                 name: name,
@@ -45,6 +65,14 @@ define(function (require, exports, module) {
         }
     }
 
+    /**
+     * Traverse a subtree recursivly.
+     * @private
+     * @param   {object}   node AST node
+     * @param   {object[]} list List of objects for the parsed nodes
+     * @param   {string}   name ame of a previous node
+     * @returns {object[]} List of objects for the parsed nodes
+     */
     function _traverse(node, list, name) {
         if (node && typeof node.type === "string") {
             var res = _visit(node, name);
@@ -74,6 +102,11 @@ define(function (require, exports, module) {
         return list;
     }
 
+    /**
+     * Parse the source and extract the code structure.
+     * @param   {string}   source the source code.
+     * @returns {object[]} the code structure.
+     */
     function parse(source) {
         var ast = esprima.parse(source, {
             loc: true,
