d06e7650b11762d73cfd3894721a257d74b17839
Jan Pilzer <jan.pilzer@gmx.de>
2017-03-13 19:33:43 +0100

    Reduce number of assertions in JS test

--- test/JavaScriptSpec.js
+++ test/JavaScriptSpec.js
@@ -7,156 +7,158 @@ define(function JavaScriptSpec(require) {
         it("detects a function declarations and expressions", function () {
             var test = require("text!example/javascript/function.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(3);
-
-            expect(result[0].args.length).toEqual(0);
-            expect(result[0].level).toEqual(0);
-            expect(result[0].line).toEqual(1);
-            expect(result[0].name).toEqual("a");
-            expect(result[0].type).toEqual("public");
-
-            expect(result[1].args.length).toEqual(0);
-            expect(result[1].level).toEqual(0);
-            expect(result[1].line).toEqual(3);
-            expect(result[1].name).toEqual("b");
-            expect(result[1].type).toEqual("public");
-
-            expect(result[2].args.length).toEqual(0);
-            expect(result[2].level).toEqual(0);
-            expect(result[2].line).toEqual(5);
-            expect(result[2].name).toEqual("d");
-            expect(result[2].type).toEqual("public");
+            expect(result).toEqual([
+                {
+                    type: "public",
+                    name: "a",
+                    args: [],
+                    level: 0,
+                    line: 1
+                }, {
+                    type: "public",
+                    name: "b",
+                    args: [],
+                    level: 0,
+                    line: 3
+                }, {
+                    type: "public",
+                    name: "d",
+                    args: [],
+                    level: 0,
+                    line: 5
+                }
+            ]);
         });
 
         it("detects an unnamed function", function () {
             var test = require("text!example/javascript/unnamedFunction.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(1);
-
-            expect(result[0].args.length).toEqual(0);
-            expect(result[0].level).toEqual(0);
-            expect(result[0].line).toEqual(1);
-            expect(result[0].name).toEqual("function");
-            expect(result[0].type).toEqual("unnamed");
+            expect(result).toEqual([
+                {
+                    type: "unnamed",
+                    name: "function",
+                    args: [],
+                    level: 0,
+                    line: 1
+                }
+            ]);
         });
 
         it("detects a class and class methods", function () {
             var test = require("text!example/javascript/classMethod.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(3);
-
-            expect(result[0].args.length).toEqual(0);
-            expect(result[0].level).toEqual(0);
-            expect(result[0].line).toEqual(1);
-            expect(result[0].name).toEqual("Name");
-            expect(result[0].type).toEqual("class");
-
-            expect(result[1].args.length).toEqual(0);
-            expect(result[1].level).toEqual(0);
-            expect(result[1].line).toEqual(3);
-            expect(result[1].name).toEqual("getString");
-            expect(result[1].type).toEqual("public");
-
-            expect(result[2].args.length).toEqual(0);
-            expect(result[2].level).toEqual(0);
-            expect(result[2].line).toEqual(5);
-            expect(result[2].name).toEqual("_getPrivateString");
-            expect(result[2].type).toEqual("private");
+            expect(result).toEqual([
+                {
+                    type: "class",
+                    name: "Name",
+                    args: [],
+                    level: 0,
+                    line: 1
+                }, {
+                    type: "public",
+                    name: "getString",
+                    args: [],
+                    level: 0,
+                    line: 3
+                }, {
+                    type: "private",
+                    name: "_getPrivateString",
+                    args: [],
+                    level: 0,
+                    line: 5
+                }
+            ]);
         });
 
         it("detects function parameters", function () {
             var test = require("text!example/javascript/functionParameters.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(2);
-
-            expect(result[0].args.length).toEqual(2);
-            expect(result[0].args[0]).toEqual("a");
-            expect(result[0].args[1]).toEqual("b");
-            expect(result[0].level).toEqual(0);
-            expect(result[0].line).toEqual(1);
-            expect(result[0].name).toEqual("withParameters");
-            expect(result[0].type).toEqual("public");
-
-            expect(result[1].args.length).toEqual(2);
-            expect(result[1].args[0]).toEqual("a=1");
-            expect(result[1].args[1]).toEqual("b=" + Parser.ARG_DEFAULT_PLACEHOLDER);
-            expect(result[1].level).toEqual(0);
-            expect(result[1].line).toEqual(3);
-            expect(result[1].name).toEqual("withDefaultParameters");
-            expect(result[1].type).toEqual("public");
+            expect(result).toEqual([
+                {
+                    type: "public",
+                    name: "withParameters",
+                    args: ["a", "b"],
+                    level: 0,
+                    line: 1
+                }, {
+                    type: "public",
+                    name: "withDefaultParameters",
+                    args: ["a=1", "b=" + Parser.ARG_DEFAULT_PLACEHOLDER],
+                    level: 0,
+                    line: 3
+                }
+            ]);
         });
 
         it("detects ES6 functions", function () {
             var test = require("text!example/javascript/ES6.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(3);
-
-            expect(result[0].args.length).toEqual(0);
-            expect(result[0].level).toEqual(0);
-            expect(result[0].line).toEqual(2);
-            expect(result[0].name).toEqual("one");
-            expect(result[0].type).toEqual("public");
-
-            expect(result[1].args.length).toEqual(0);
-            expect(result[1].level).toEqual(0);
-            expect(result[1].line).toEqual(3);
-            expect(result[1].name).toEqual("two");
-            expect(result[1].type).toEqual("public");
-
-            expect(result[2].args.length).toEqual(0);
-            expect(result[2].level).toEqual(0);
-            expect(result[2].line).toEqual(4);
-            expect(result[2].name).toEqual("three");
-            expect(result[2].type).toEqual("public");
+            expect(result).toEqual([
+                {
+                    type: "public",
+                    name: "one",
+                    args: [],
+                    level: 0,
+                    line: 2
+                }, {
+                    type: "public",
+                    name: "two",
+                    args: [],
+                    level: 0,
+                    line: 3
+                }, {
+                    type: "public",
+                    name: "three",
+                    args: [],
+                    level: 0,
+                    line: 4
+                }
+            ]);
         });
 
         it("detects generator functions", function () {
             var test = require("text!example/javascript/generator.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(1);
-
-            expect(result[0].args.length).toEqual(1);
-            expect(result[0].level).toEqual(0);
-            expect(result[0].line).toEqual(1);
-            expect(result[0].name).toEqual("generator");
-            expect(result[0].type).toEqual("generator");
+            expect(result).toEqual([
+                {
+                    type: "generator",
+                    name: "generator",
+                    args: ["i"],
+                    level: 0,
+                    line: 1
+                }
+            ]);
         });
 
         it("detects nested functions", function () {
             var test = require("text!example/javascript/nestedFunctions.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(3);
-
-            expect(result[0].args.length).toEqual(0);
-            expect(result[0].level).toEqual(0);
-            expect(result[0].line).toEqual(1);
-            expect(result[0].name).toEqual("a");
-            expect(result[0].type).toEqual("public");
-
-            expect(result[1].args.length).toEqual(0);
-            expect(result[1].level).toEqual(1);
-            expect(result[1].line).toEqual(2);
-            expect(result[1].name).toEqual("b");
-            expect(result[1].type).toEqual("public");
-
-            expect(result[2].args.length).toEqual(0);
-            expect(result[2].level).toEqual(2);
-            expect(result[2].line).toEqual(3);
-            expect(result[2].name).toEqual("c");
-            expect(result[2].type).toEqual("public");
+            expect(result).toEqual([
+                {
+                    type: "public",
+                    name: "a",
+                    args: [],
+                    level: 0,
+                    line: 1
+                }, {
+                    type: "public",
+                    name: "b",
+                    args: [],
+                    level: 1,
+                    line: 2
+                }, {
+                    type: "public",
+                    name: "c",
+                    args: [],
+                    level: 2,
+                    line: 3
+                }
+            ]);
         });
 
         it("detects es6 classes", function () {
             var test = require("text!example/javascript/class.js");
             var result = Parser.parse(test);
-
             expect(result).toEqual([
                 {
                     type: "class",
@@ -195,8 +197,7 @@ define(function JavaScriptSpec(require) {
         it("detects comments", function () {
             var test = require("text!example/javascript/comment.js");
             var result = Parser.parse(test);
-
-            expect(result.length).toEqual(0);
+            expect(result).toEqual([]);
         });
 
         it("error", function () {
