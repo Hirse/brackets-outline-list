063620364df5f1c9f3ba7155cbb573de00a3e175
pelatx <pelatx@gmail.com>
2017-03-15 11:41:15 +0100

    Preference for AutoHide feature.

--- src/Preferences.js
+++ src/Preferences.js
@@ -65,6 +65,15 @@ define(function (require, exports, module) {
                 description: Strings.PREF_INDENT_DESC
             },
             inContextMenu: true
+        }, {
+            id: "autohide",
+            type: "boolean",
+            value: true,
+            options: {
+                name: Strings.PREF_AUTOHIDE_NAME,
+                description: Strings.PREF_AUTOHIDE_DESC
+            },
+            inContextMenu: true
         }
     ];
 
