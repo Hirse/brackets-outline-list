da3b074f698676d211def76102b0e8401838d157
Michał Korczak <michal.k2001@gmail.com>
2017-08-23 15:27:11 +0200

    Polish translation (#95)

---
+++ nls/pl/strings.js
@@ -0,0 +1,32 @@
+define({
+    HEADER_TITLE: "Zarys",
+
+    TOOLBAR_ICON_TOOLTIP: "Przełącz zarys",
+
+    BUTTON_SETTINGS: "Konfiguruj zarys",
+    BUTTON_MOVE: "Przełącz pozycję zarysu",
+    BUTTON_CLOSE: "Zamknij zarys",
+
+    COMMAND_SORT: "Sortuj wpisy",
+    COMMAND_UNNAMED: "Pokaż nienazwane funkcje",
+    COMMAND_ARGS: "Pokaż argumenty",
+    COMMAND_INDENT: "Wcięcia w wpisach",
+    COMMAND_OUTLINE: "Autoukrywanie listy zarysu",
+
+    MESSAGE_SYNTAX_ERROR: "Napraw błędy składni w pliku, aby pokazać zarys.",
+
+    PREF_ENABLED_NAME: "Zarys - Włączony",
+    PREF_ENABLED_DESC: "Włącz/wyłącz zarys",
+    PREF_UNNAMED_NAME: "Zarys - Nienazwany",
+    PREF_UNNAMED_DESC: "Pokaż/ukryj nienazwane funkcje w liście zarysu",
+    PREF_ARGS_NAME: "Zarys - argumenty",
+    PREF_ARGS_DESC: "Pokaż/ukryj parametry funkcji i klasy HTML w liście zarysu",
+    PREF_SIDEBAR_NAME: "Zarys - Pasek boczny",
+    PREF_SIDEBAR_DESC: "Tak, aby pokazać listę zarysu w pasku bocznym poniżej drzewa plików",
+    PREF_SORT_NAME: "Zarys - Sortowanie",
+    PREF_SORT_DESC: "Tak, aby sortować wpisy w liście zarysu",
+    PREF_INDENT_NAME: "Zarys - Wcięcia",
+    PREF_INDENT_DESC: "Tak, aby używać wcięć we wpisach w liście zarysu",
+    PREF_AUTOHIDE_NAME: "Zarys - Autoukrywanie",
+    PREF_AUTOHIDE_DESC: "Tak, aby włączyć autoukrywanie listy zarysu"
+});
