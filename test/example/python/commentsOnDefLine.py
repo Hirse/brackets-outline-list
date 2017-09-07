83a8b91dbd90aae5bea4160560fe2138574da91a
pelatx <pelatx@gmail.com>
2017-09-07 14:24:05 +0200

    Added some Python comment examples more.

--- test/example/python/commentsOnDefLine.py
+++ test/example/python/commentsOnDefLine.py
@@ -3,3 +3,9 @@ def plot1(ax, X=None, C=None):
 
 def plot2(ax, X=None, C=None): #some comment
     pass
+
+def plot3(ax, X=None, C=None):####     Some Comment
+    pass
+
+def plot4(ax, X=None, C=None):          #   $0M€ %&/()!|\[]'?¿COMMENT
+    pass
